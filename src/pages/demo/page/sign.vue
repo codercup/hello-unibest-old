c6f02395a2fdfb5f23ff7183128a84ddf1f96cc6
summer <tgj98@qq.com>
2024-02-01 11:07:28 +0800

    feat: 添加签字版组件

---
+++ src/pages/demo/page/sign.vue
@@ -0,0 +1,265 @@
+<route lang="json5">
+{
+  layout: 'default',
+  style: { navigationBarTitleText: '签字板' },
+}
+</route>
+
+<template>
+  <view
+    class="canvas-box flex flex-col box-border"
+    :class="full ? 'full-screen py30rpx' : 'px20rpx py20rpx'"
+  >
+    <canvas
+      canvas-id="canvas"
+      class="w-full b b-dashed b-rd b-gray-300 canvas"
+      ref="canvasRef"
+      disable-scroll="true"
+      @touchstart="touchStart"
+      @touchmove="touchMove"
+      @touchend="touchEnd"
+      @mousedown="touchStart"
+      @mousemove="touchMove"
+      @mouseup="touchEnd"
+    />
+    <view class="btns flex justify-between text-center box-border">
+      <view class="btn-box flex">
+        <view class="btn bg-gray-100 b-rd b-gray-300 c-gray-500 p-1" @click="handFullScreen"
+          >全屏</view
+        >
+        <view class="btn bg-gray-100 b-rd b-gray-300 c-gray-500 p-1 ml-2" @click="clear">清空</view>
+        <view class="btn bg-gray-100 b-rd b-gray-300 c-gray-500 p-1 ml-2" @click="withdraw"
+          >撤回</view
+        >
+      </view>
+      <view class="btn-box flex">
+        <view class="btn bg-sky-500 b-rd c-white p-1" @click="save">保存</view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script lang="ts" setup name="sign">
+const canvasRef = ref(null)
+const full = ref(false)
+let ctx = null
+let isButtonDown = false
+let points = []
+let allPoints = []
+const isSigned = ref(false)
+
+function initCanvas() {
+  const W = canvasRef.value.$el.offsetWidth
+  const h = canvasRef.value.$el.offsetHeight
+
+  ctx = uni.createCanvasContext('canvas', this)
+  // 设置画笔样式
+
+  ctx.rect(0, 0, W, h)
+  ctx.lineWidth = 4
+  ctx.lineCap = 'round'
+  ctx.lineJoin = 'round'
+}
+
+onMounted(() => {
+  initCanvas()
+})
+
+// 重设画板大小
+function onResize() {
+  initCanvas() // 重新初始化canvas
+}
+
+// 绘画
+function draw(w?) {
+  const point1 = points[0]
+  const point2 = points[1]
+
+  if (!w) {
+    allPoints[allPoints.length - 1].push(JSON.parse(JSON.stringify(points)))
+  }
+
+  points.shift()
+  ctx.moveTo(point1.X, point1.Y)
+  ctx.lineTo(point2.X, point2.Y)
+  ctx.stroke()
+  ctx.draw(true)
+  isSigned.value = true
+}
+
+// 触摸开始，获取到起点
+function touchStart() {
+  allPoints.push([])
+  ctx.beginPath() // 每次触摸开始，开启新的路径
+  isButtonDown = true
+}
+
+// 触摸移动，获取到路径点
+function touchMove(e) {
+  if (isButtonDown) {
+    const movePoint = { X: e.changedTouches[0].x, Y: e.changedTouches[0].y }
+    points.push(movePoint) // 存点
+    const len = points.length
+    if (len >= 2) {
+      draw() // 绘制路径
+    }
+  }
+}
+
+// 触摸结束，将未绘制的点清空防止对后续路径产生干扰
+function touchEnd() {
+  allPoints = allPoints.filter((e) => {
+    return e.length > 0
+  })
+  points = []
+  isButtonDown = false
+}
+
+// 清空
+function clear(w?) {
+  if (w !== 1) allPoints = []
+  ctx.clearRect(0, 0, 1000, 1000)
+  ctx.draw(true)
+  isSigned.value = false
+}
+
+// 全屏
+function handFullScreen() {
+  clear()
+  full.value = !full.value
+  setTimeout(() => {
+    onResize()
+  }, 100)
+}
+
+// 撤回
+function withdraw() {
+  // 清除画布
+  clear(1)
+  if (allPoints.length <= 1) {
+    allPoints = []
+    points = []
+    return
+  }
+  // 删除最后一个路径
+  allPoints.pop()
+  // 循环路径，重新绘制
+  allPoints.forEach((e) => {
+    e.forEach((r) => {
+      points = JSON.parse(JSON.stringify(r))
+      draw(1)
+    })
+  })
+}
+
+// h5保存方法
+function saveCanvasAsImage(dataURL, imageName?) {
+  // 创建一个Image元素
+  const img = new Image()
+
+  // 设置img的src属性为数据URL
+  img.src = dataURL
+
+  // 创建一个链接元素用于下载图片
+  const link = document.createElement('a')
+
+  // 设置下载的文件名
+  link.download = imageName || 'canvas-image'
+
+  // 触发点击，下载图片
+  link.href = img.src
+  link.click()
+}
+
+// 保存
+const save = () => {
+  if (!unref(isSigned)) {
+    uni.showToast({
+      title: '请签名',
+      icon: 'none',
+    })
+    return
+  }
+
+  uni.canvasToTempFilePath({
+    canvasId: 'canvas',
+    success: (res) => {
+      // 获取图片路径
+      const { tempFilePath } = res
+      // 保存图片到相册
+      // #ifdef H5
+      const name = `sign-${new Date().getTime()}`
+      saveCanvasAsImage(tempFilePath, name)
+      // #endif
+      // #ifndef H5
+      uni.saveImageToPhotosAlbum({
+        filePath: tempFilePath,
+        success: () => {
+          uni.showToast({
+            title: '图片保存成功',
+          })
+        },
+        fail: () => {
+          uni.showToast({
+            title: '图片保存失败',
+            icon: 'none',
+          })
+        },
+      })
+      // #endif
+    },
+    fail: () => {
+      uni.showToast({
+        title: '转换图片失败',
+        icon: 'none',
+      })
+    },
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+$padding: 30rpx;
+
+.canvas-box {
+  .canvas {
+    height: 300rpx;
+    transition: height 0.3s;
+  }
+
+  .btns {
+    margin-top: 10rpx;
+    transition: transform 0.3s;
+
+    .btn {
+      width: auto;
+      height: 50rpx;
+    }
+  }
+}
+
+.full-screen {
+  flex-direction: row;
+  height: calc(100vh - 88rpx);
+
+  .canvas {
+    width: calc(100% - 130rpx);
+    height: 100%;
+    margin-right: 30rpx;
+    margin-left: 100rpx;
+  }
+
+  .btns {
+    position: absolute;
+    align-items: center;
+    width: calc(100vh - (88rpx + $padding * 2));
+    height: 100rpx;
+    transform: translate(100rpx, 0) rotate(90deg);
+    transform-origin: top left;
+
+    .btn-box {
+      flex-direction: row;
+    }
+  }
+}
+</style>
